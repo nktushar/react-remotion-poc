6cd87c02a0bb78b8a9aba614c72d3091244f2793
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2024-07-08 02:06:43 +0600

    test video created

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,7 @@
 import {Composition} from 'remotion';
 import {HelloWorld} from './HelloWorld';
 import {Logo} from './HelloWorld/Logo';
+import {MyVideo} from './TestVideo';
 
 // Each <Composition> is an entry in the sidebar!
 
@@ -28,6 +29,14 @@ export const RemotionRoot = () => {
 				id="OnlyLogo"
 				component={Logo}
 				durationInFrames={150}
+				fps={30}
+				width={1920}
+				height={1080}
+			/>
+			<Composition
+				id="TestVideo"
+				component={MyVideo}
+				durationInFrames={90}
 				fps={30}
 				width={1920}
 				height={1080}
---
+++ src/TestVideo/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {interpolate, useCurrentFrame} from 'remotion';
+
+export const MyVideo = () => {
+	const frame = useCurrentFrame();
+	const opacity = interpolate(frame, [0, 30], [0, 1]);
+
+	return (
+		<div
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				display: 'flex',
+				backgroundColor: 'cyan',
+				fontSize: 100,
+				fontFamily: 'Arial',
+			}}
+		>
+			<h1 style={{opacity}}>Hello, Remotion!</h1>
+		</div>
+	);
+};
